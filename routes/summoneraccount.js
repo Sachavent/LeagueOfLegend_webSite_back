7808e9fe730bc8b6d222186747333f30a262a7da
Sachavent <s.ventribout@gmail.com>
2017-03-05 00:14:33 +0100

    testing url resolve

--- routes/summoneraccount.js
+++ routes/summoneraccount.js
@@ -32,8 +32,11 @@ router.get('/:summonername/global', function (req, response) {
  */
 
 router.get('/:compteid/rank', function (req, response) {
+    console.log("ici: ");
     var compteid = req.params.compteid;
-    let url = 'https://euw.api.pvp.net/api/lol/euw/v2.5/league/by-summoner/${compteid}/entry?api_key=RGAPI-650e27b6-8c7d-490b-a47d-afabc202e5b7'
+    console.log("compteid: " + compteid);
+    let url = `https://euw.api.pvp.net/api/lol/euw/v2.5/league/by-summoner/${compteid}/entry?api_key=RGAPI-650e27b6-8c7d-490b-a47d-afabc202e5b7`
+    console.log("url: " + url);
 
     var req = https.get(url, function (res) {
         //console.log('statusCode:', res.statusCode);
@@ -49,6 +52,7 @@ router.get('/:compteid/rank', function (req, response) {
     });
 
     req.end();
+    response.end("coucou")
 });
 
 /**
@@ -56,9 +60,9 @@ router.get('/:compteid/rank', function (req, response) {
  */
 router.get('/:compteid/mostchampionsplayed', function (req, response) {
     var compteid = req.params.compteid;
-    let url = 'https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/${compteid}/ranked?api_key=RGAPI-650e27b6-8c7d-490b-a47d-afabc202e5b7'
+    let url = `https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/${compteid}/ranked?api_key=RGAPI-650e27b6-8c7d-490b-a47d-afabc202e5b7`
 
-        var req = https.get(url, function (res) {
+    var req = https.get(url, function (res) {
         //console.log('statusCode:', res.statusCode);
         //console.log('headers:', res.headers);
         res.on('data', (d) => {
