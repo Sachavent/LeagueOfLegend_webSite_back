f725aa1bd6e3a9a5f085897b7f4bd9917cd3a9e1
Sachavent <s.ventribout@gmail.com>
2017-03-04 21:26:43 +0100

    serveur listening on env.port

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(function(req, res, next) {
 app.use('/summoneraccount', require('./routes/summoneraccount'))
 
 
-app.listen(3000, function () {
+app.listen( process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
