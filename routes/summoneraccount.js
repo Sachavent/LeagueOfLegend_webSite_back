8f39e6d5ab7d94aa35ae58ec04854b414b28056f
Sachavent <s.ventribout@gmail.com>
2017-03-05 12:58:09 +0100

    adding res.end on "end" event

--- routes/summoneraccount.js
+++ routes/summoneraccount.js
@@ -56,15 +56,19 @@ router.get('/:compteid/rank', function (req, response) {
 /**
  * Get the list of the most played champion
  */
-/*router.get('/:compteid/mostchampionsplayed', function (req, response) {
+router.get('/:compteid/mostchampionsplayed', function (req, response) {
     var compteid = req.params.compteid;
     let url = `https://euw.api.pvp.net/api/lol/euw/v1.3/stats/by-summoner/${compteid}/ranked?api_key=RGAPI-650e27b6-8c7d-490b-a47d-afabc202e5b7`
     var req = https.get(url, function (res) {
         console.log('statusCode:', res.statusCode);
         console.log('headers:', res.headers);
         res.on('data', (d) => {
-           response.send(d)
+           response.write(d)
         });
+
+        res.once("end", () => {
+            response.end();
+        })
     });
 
     req.on('error', (e) => {
@@ -74,6 +78,6 @@ router.get('/:compteid/rank', function (req, response) {
     req.end();
 
 
-});*/
+});
 
 module.exports = router;
